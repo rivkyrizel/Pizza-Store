91484e289d76e58a3b63611e9b60a7818dc1a40c
visual studio <vs@mbjcomp.org.il>
2022-09-19 14:14:30 +0300

    initial welcome2774

---
+++ dotNet5783_2774_6645/Stage0/Program2774.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        private static void Welcome2774()
+        {
+            Console.WriteLine("I am also here! ");
+           
+        }
+    }
+}
