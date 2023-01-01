4a7fd1ba84f81aeabb9e7d740ddb3db5927f22a0
rivkyrizel <rr0583238196@gmail.com>
2023-01-01 14:51:17 +0200

    create Dal xml project

---
+++ dotNet5783_2774_6645/DalXml/DalXml.cs
@@ -0,0 +1,11 @@
+﻿namespace Dal;
+using DalApi;
+using DO;
+
+sealed internal class DalXml : IDal
+{
+    public IProduct Product { get ; } = new Dal.Product();
+    public IOrder Order { get ; } = new Dal.Order();
+    public IOrderItem OrderItem { get; } = new Dal.OrderItem();
+
+}
