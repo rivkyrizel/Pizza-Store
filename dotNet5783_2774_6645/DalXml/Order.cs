55e11575e195f0be131320ada4ecf840d814dbe6
rivky rizel <rr0583238196@gmail.com>
2023-01-03 20:21:39 +0200

    get signal in order

--- dotNet5783_2774_6645/DalXml/Order.cs
+++ dotNet5783_2774_6645/DalXml/Order.cs
@@ -41,25 +41,33 @@ public class Order : IOrder
     public DO.Order Get(Func<DO.Order, bool> func)
     {
         IEnumerable<XElement> ie = root.Elements("Order");
+
+        IEnumerable<XElement> orderElements = from v in root.Elements()
+                                              select v;
         DO.Order d = new();
-        IEnumerable<DO.Order> i = from v in ie.Elements()
-                                  select (
-                                          from element in v.Elements()
-                                          select new
-                                          {
-                                           { d.ID}   
-                                          }
-                                          );
-          //select (d.GetType().GetProperty(element.ToString()).SetValue(d, element.Value)));
-
-        throw new Exception("hii");
+        var e = from orderElement in orderElements
+                from element in orderElement.Elements()
+                select func2(ref d, element);
 
 
+        ////from l in element.Elements()
+        ////select new { element, l.Name, l.Value };
+        //foreach (XElement o in orderElements)
+        //{
+
+        //    foreach (XElement mx in o.Elements())
+        //    {
+        //        func2(ref d, mx);
+        //    }
+        //}
+        throw new Exception("hhh");
     }
-    private XElement func2(DO.Order d, XElement element)
+
+    private int func2(ref DO.Order d, XElement element)
     {
-        d.GetType().GetProperty(element.ToString()).SetValue(d, element.Value);
-        return element;
+        if (element.Name.ToString() != "ID")
+            d.GetType().GetProperty(element.Name.ToString()).SetValue(d, element.Value);
+        return 2;
     }
     public IEnumerable<DO.Order>? GetList(Func<DO.Order, bool>? func = null)
     {
@@ -68,14 +76,16 @@ public class Order : IOrder
 
     public void Update(DO.Order order)
     {
+
+
         XElement? o = new("Order",
-                        new XElement("ID", order.ID),
-                        new XElement("CustomerName", order.CustomerName),
-                        new XElement("CustomerEmail", order.CustomerEmail),
-                        new XElement("CustomerAddress", order.CustomerAddress),
-                        new XElement("OrderDate", order.OrderDate),
-                        new XElement("ShipDate", order.ShipDate),
-                        new XElement("DeliveryDate", order.DeliveryDate));
+                            new XElement("ID", order.ID),
+                            new XElement("CustomerName", order.CustomerName),
+                            new XElement("CustomerEmail", order.CustomerEmail),
+                            new XElement("CustomerAddress", order.CustomerAddress),
+                            new XElement("OrderDate", order.OrderDate),
+                            new XElement("ShipDate", order.ShipDate),
+                            new XElement("DeliveryDate", order.DeliveryDate));
         root?.Elements("Order")?.Where(o => int.Parse(o.Element("ID").Value.ToString()) == order.ID)?.FirstOrDefault()?.ReplaceWith(o);
         root?.Save(@"..\..\..\..\..\xml\Order.xml");
     }
