d562df8cb623fd1c9ee18a86f22d3f6618974267
rivkyrizel <rr0583238196@gmail.com>
2023-01-08 16:41:05 +0200

    add Exception

--- dotNet5783_2774_6645/BL/BlImplementation/BlUtils.cs
+++ dotNet5783_2774_6645/BL/BlImplementation/BlUtils.cs
@@ -7,12 +7,12 @@ internal static class BlUtils
     internal static S cast<S, T>(T t) where S : new()
     {
         object s = new S();
-        foreach (PropertyInfo prop in t?.GetType().GetProperties()??throw new Exception())
+        foreach (PropertyInfo prop in t?.GetType().GetProperties()??throw new Exception("the object doesn't have properties"))
         {
-            PropertyInfo type = s?.GetType().GetProperty(prop.Name)?? throw new Exception();
+            PropertyInfo type = s?.GetType().GetProperty(prop.Name)?? throw new Exception("the object doesn't have properties");
             if (type == null || type.Name == "Category" )
                 continue;
-            var value = t?.GetType()?.GetProperty(prop.Name)?.GetValue(t, null) ??throw new Exception();
+            var value = t?.GetType()?.GetProperty(prop.Name)?.GetValue(t, null) ??throw new Exception("the object doesn't have properties");
             type.SetValue(s, value);
         }
         return (S)s;
